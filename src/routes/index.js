5c3ffc5badd966a0ea65182747f9cdd8ed935292
Diego Pezet <74683374+diegohpezet@users.noreply.github.com>
2023-06-17 13:10:46 -0300

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,17 +4,22 @@ const User = require('../models/user')
 
 router.get('/', (req, res) => res.render('index.ejs', {registerFailed: false}))
 router.post('/register', (req,res) => {
-    const { username, email, password } = req.body;
+    const { username, email, password, repeatpassword } = req.body;
 
-    const user = new User({username, email, password})
-    user.save(err => {
-        if(err) {
-            res.render('index', {regMessage: "Username already taken!", registerFailed: true})
-            console.log(err)
-        } else {
-            res.render('main', {data: user})
-        }
-    })
+    if (password === repeatpassword) {
+        const user = new User({username, email, password})
+        user.save(err => {
+            if(err) {
+                res.render('index', {regMessage: "Username already taken!", registerFailed: true})
+                console.log(err)
+            } else {
+                res.render('main', {data: user})
+            }
+        })
+    } else {
+        res.render('index', {regMessage: "Passwords don't match!", registerFailed: true})
+    }
+    
 })
 router.post('/authenticate', (req, res) => {
     const {username, password} = req.body;
@@ -22,6 +27,7 @@ router.post('/authenticate', (req, res) => {
     User.findOne({username}, (err, user) => {
         if(err) {
             res.render('index', {loginMessage: "User authentication error"})
+            console.log(err);
         } else if (!user) {
             res.render('index', {loginMessage: "User does not exist"})
         } else {
