bbc923f5ee83fee9dacc99401d37144d32116a7b
Diego Pezet <74683374+diegohpezet@users.noreply.github.com>
2023-06-18 17:39:21 -0300

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,47 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user')
+const User = require("../models/user");
 
-router.get('/', (req, res) => res.render('index.ejs', {registerFailed: false}))
-router.post('/register', (req,res) => {
-    const { username, email, password, repeatpassword } = req.body;
+router.get("/", (req, res) =>
+  res.render("index.ejs", { registerFailed: false })
+);
+router.post("/register", (req, res) => {
+  const { username, email, password, repeatpassword } = req.body;
 
+  if (password.length > 8) {
     if (password === repeatpassword) {
-        const user = new User({username, email, password})
-        user.save(err => {
-            if(err) {
-                res.render('index', {regMessage: "Username already taken!", registerFailed: true})
-                console.log(err)
-            } else {
-                res.render('main', {data: user})
-            }
-        })
-    } else {
-        res.render('index', {regMessage: "Passwords don't match!", registerFailed: true})
-    }
-    
-})
-router.post('/authenticate', (req, res) => {
-    const {username, password} = req.body;
-
-    User.findOne({username}, (err, user) => {
-        if(err) {
-            res.render('index', {loginMessage: "User authentication error"})
-            console.log(err);
-        } else if (!user) {
-            res.render('index', {loginMessage: "User does not exist"})
+      const user = new User({ username, email, password });
+      user.save((err) => {
+        if (err) {
+          res.render("index", {
+            regMessage: "Username already taken!",
+            registerFailed: true,
+          });
+          console.log(err);
         } else {
-            user.comparePassword(password, (err, result) => {
-                if(err){
-                    res.render('index', {loginMessage: "Authentication error"})
-                } else if (result) {
-                    res.render('main', {data: user});
-                } else {
-                    res.render('index', {loginMessage: "Invalid password"})
-                }
-            });
+          res.render("main", { data: user });
         }
-    })
-})
+      });
+    } else {
+      res.render("index", {
+        regMessage: "Passwords don't match",
+        registerFailed: true,
+      });
+    }
+  } else {
+    res.render("index", {
+      regMessage: "Password must contain more than 8 characters",
+      registerFailed: true,
+    });
+  }
+});
+
+
+router.post("/authenticate", (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      res.render("index", { loginMessage: "User authentication error" });
+      console.log(err);
+    } else if (!user) {
+      res.render("index", { loginMessage: "User does not exist" });
+    } else {
+      user.comparePassword(password, (err, result) => {
+        if (err) {
+          res.render("index", { loginMessage: "Authentication error" });
+        } else if (result) {
+          res.render("main", { data: user });
+        } else {
+          res.render("index", { loginMessage: "Invalid password" });
+        }
+      });
+    }
+  });
+});
 
 module.exports = router;
